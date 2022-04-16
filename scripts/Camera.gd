39279bcf3e542c3b6687625565c972c67553a0a3
Ayman Syam <ayman.syam@gmail.com>
2022-04-16 18:52:47 +0300

    Added camera control + relative movement

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,8 +1,22 @@
 extends Camera
 
+var current_camera_loc = get_parent().translation + Vector3(5, 5, 5)
 
 func _ready():
-	look_at_from_position(get_parent().translation + Vector3(5, 5, 5), get_parent().translation, Vector3(0, 1, 0))
+	look_at_from_position(current_camera_loc, get_parent().translation, Vector3(0, 1, 0))
 	
 func _physics_process(delta):
-	look_at_from_position(get_parent().translation + Vector3(5, 5, 5), get_parent().translation, Vector3(0, 1, 0))
+	look_at_from_position(current_camera_loc, get_parent().translation, Vector3(0, 1, 0))
+	
+func _input(event):
+   # Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+		current_camera_loc = 
+		
+		
+
+   # Print the size of the viewport.
+	print("Viewport Resolution is: ", get_viewport().size)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,26 +4,64 @@ extends KinematicBody
 export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 export var fall_acceleration = 75
+export var jump_impulse = 30
 
 var velocity = Vector3.ZERO 
 
+var camNode = null
+
+func _ready():
+	camNode = get_node("ThirdPersonCamera")
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
-
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		direction.z += 1
-	if Input.is_action_pressed("ui_down"):
-		direction.z -= 1
-
+	var camDirection = camNode.translation
+	var camDirection2d = Vector2(-1 * camDirection.x, -1 * camDirection.z)
+	
+	if is_forward_pressed() and is_right_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 45)
+	elif is_forward_pressed() and is_left_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 315)
+	elif is_back_pressed() and is_left_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 225)
+	elif is_back_pressed() and is_right_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 135)
+	elif is_right_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 90)
+	elif is_left_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 270)
+	elif is_forward_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 0)
+	elif is_back_pressed():
+		direction = rotate2DRelativeTo(camDirection2d, 180)
+		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
+	
+	if is_on_floor() and Input.is_action_just_pressed("ui_select"):
+		velocity.y = jump_impulse
+	
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+
+func is_forward_pressed():
+	return Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W)
+	
+func is_left_pressed():
+	return Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A)
+	
+func is_right_pressed():
+	return Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D)
+	
+func is_back_pressed():
+	return Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S)
+	
+func rotate2DRelativeTo(vector, number):
+	var new_vec = Vector3.ZERO
+	new_vec.x = vector.rotated(deg2rad(number)).x
+	new_vec.z = vector.rotated(deg2rad(number)).y
+	new_vec.y = vector.y
+	return new_vec
